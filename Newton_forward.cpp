2ee8f58e37de9cf2c13b08ab85a1872c77dd310e
Rifat_Sayeed <rifatahmed21095@gmail.com>
2026-05-05 13:43:54 -0400

    Refactor Newton forward interpolation implementation

--- Newton_forward.cpp
+++ Newton_forward.cpp
@@ -1,49 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-int main () {
+int main() {
+    int n;
+    double x[20], y[20];
 
-float n , x[10], y[10];
-cout <<"Enter number of n : " ; cin>> n;
+    cout << "Enter number of data points: ";
+    cin >> n;
 
-cout <<"Enter values x and y : " ; 
-
-for (int i=0 ; i<n ; i++) {
-
-    cin>>x[i]>>y[i];
-
-}
-
-float t, h, u ,d ,p=1.00  ;
-
-cout <<"Enter target: " ; cin>> t;
-
-
-
-
-h= (x[1]-x[0]);
-
-u=(t-x[0])/h;
-
-d=y[0];
-
-for (int i=1 ; i<n ; i++) {
-
-    for (int j=0;j<n-i;j++ ){
-
-        y[j]=y[j+1]-y[j];
-       
+    cout << "Enter x and y values:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
     }
 
-    p= p* (u-i+1)/i;
+    double target;
+    cout << "Enter target year: ";
+    cin >> target;
 
-d=d+ p*y[0];
+    double h = x[1] - x[0];
+    double u = (target - x[0]) / h;
 
-}
+    double ans = y[0];
+    double p = 1;
 
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            y[j] = y[j + 1] - y[j];
+        }
 
+        p = p * (u - i + 1) / i;
+        ans = ans + p * y[0];
+    }
 
-cout<< "X = " << t << " y = " << d ;
+    cout << "Population = " << ans << " thousand";
 
-
+    return 0;
 }
