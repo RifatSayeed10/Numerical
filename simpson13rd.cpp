bf84cb097b1f381e1dfec442018949624a7658c1
Rifat_Sayeed <rifatahmed21095@gmail.com>
2026-05-05 13:45:19 -0400

    Update simpson13rd.cpp

--- simpson13rd.cpp
+++ simpson13rd.cpp
@@ -1,33 +1,42 @@
-#include<bits/stdc++.h>
-#define f(x) 1/(1+x*x)
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main()
 
-
-{
-
-    double a ,b  ,n  ,xi,h        ;
-
-cout << " lower a and upper b " ; cin>>a>>b;
-
-cout <<"Enter the range : " ; cin>>n;
-
-double integral = f(a) + f(b);
-
-h=(b-a)/n;
-
-for (int i=1; i <n ; i++){
-
-     xi=a+i*h;
-
-     if (i%2==0)
-     integral=integral + 2*f(xi);
-     else
-     integral=integral + 4*f(xi);
+double f(double x) {
+    return sqrt(1 - x*x);
 }
-        
-integral=integral*(h/3);
 
-cout <<integral;
+int main() {
+    double a, b, h, sum;
+    int n;
 
+    cout << "Enter lower limit: ";
+    cin >> a;
+
+    cout << "Enter upper limit: ";
+    cin >> b;
+
+    cout << "Enter number of intervals even n: ";
+    cin >> n;
+
+    if (n % 2 != 0) {
+        cout << "n must be even!";
+        return 0;
+    }
+
+    h = (b - a) / n;
+    sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i++) {
+        if (i % 2 == 0)
+            sum += 2 * f(a + i * h);
+        else
+            sum += 4 * f(a + i * h);
+    }
+
+    double result = (h / 3) * sum;
+
+    cout << "Integral value = " << result;
+
+    return 0;
 }
